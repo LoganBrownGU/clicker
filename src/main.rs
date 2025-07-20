bd3b0733790b6edb90979628615e8d71f29fb81c
LoganBrownGU <2641407b@student.gla.ac.uk>
2025-07-20 16:46:56 +0100

    UI ooo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use std::{process::Command, thread, io, time::Duration};
+use std::{cmp::min, fmt::format, io, process::Command, thread, time::Duration};
 use tui::{
-    backend::{Backend, CrosstermBackend},
-    widgets::{Widget, Block, Borders, Paragraph},
-    layout::{Layout, Constraint, Direction},
-    Terminal,
-    Frame,
-    text,
+    backend::{Backend, CrosstermBackend}, layout::{Constraint, Direction, Layout}, style::{Color, Style}, text, widgets::{Block, BorderType, Borders, Paragraph, Widget}, Frame, Terminal
 };
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
@@ -25,45 +20,87 @@ impl GameState {
     }
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>) {
-   let chunks = Layout::default()
+fn format_float(f: f64) -> String {
+    if f > 1e4 {
+        format!("{:.3E}", f)
+    } else {
+        format!("{:.2}", f)
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, state: &GameState) {
+    let mut area = f.size().clone();
+    area.height = 12;
+
+    let [score_row, other_row] = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
         .constraints(
             [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10)
+                Constraint::Percentage(50),
+                Constraint::Percentage(50),
             ].as_ref()
         )
-        .split(f.size());
-    let block = Block::default()
-         .title("Block")
-         .borders(Borders::ALL);
-    let para = Paragraph::new("hello").block(block);
-    f.set_cursor(20, 20);
-    f.render_widget(para, chunks[0]);
+        .split(area)[..2] else { todo!() };
 
+    let score_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Max(20),   
+                Constraint::Max(20),   
+                Constraint::Max(20),   
+                Constraint::Max(10),   
+            ].as_ref()
+        )
+        .split(score_row);
 
-    let block = Block::default()
-         .title("Block 2")
-         .borders(Borders::ALL);
-    f.render_widget(block, chunks[1]);
+    let score_block = Block::default()
+        .title("Score")
+        .borders(Borders::ALL)
+        .border_style(Style::default().fg(Color::White))
+        .border_type(BorderType::Rounded)
+        .style(Style::default().bg(Color::Black));
+
+    let idle_increase_block = Block::default()
+        .title("Idle increase")
+        .borders(Borders::ALL)
+        .border_style(Style::default().fg(Color::White))
+        .border_type(BorderType::Rounded)
+        .style(Style::default().bg(Color::Black));
+
+    let active_increase_block = Block::default()
+        .title("Active increase")
+        .borders(Borders::ALL)
+        .border_style(Style::default().fg(Color::White))
+        .border_type(BorderType::Rounded)
+        .style(Style::default().bg(Color::Black));
+
+    f.render_widget(Paragraph::new(format_float(state.score)).block(score_block), score_chunks[0]);
+    f.render_widget(Paragraph::new(format_float(state.idle_increase)).block(idle_increase_block), score_chunks[1]);
+    f.render_widget(Paragraph::new(format_float(state.active_increase)).block(active_increase_block), score_chunks[2]);
 }
 
 fn main() -> Result<(), io::Error> {
-    let mut state = GameState{ score: 0.0, active_increase: 1.0, idle_increase: 1.0 };
+    let mut state = GameState{ score: 0.0, active_increase: 1.0, idle_increase: 80.12 };
     
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     
-    Command::new("clear").spawn();
+    let _ = Command::new("clear").spawn();
 
-    terminal.draw(|f| ui(f));
 
-    thread::sleep(Duration::from_millis(5000));
-    
+    loop {
+        let result = terminal.draw(|f| ui(f, &state));
+        if result.is_err() { break; }
+        
+        state.update();
+
+        thread::sleep(Duration::from_millis(16));
+    }   
+
     // restore terminal
     disable_raw_mode()?;
     execute!(
@@ -73,7 +110,7 @@ fn main() -> Result<(), io::Error> {
     )?;
     terminal.show_cursor()?;
 
-    Command::new("clear").spawn();
+    let _ = Command::new("clear").spawn();
 
     Ok(()) 
 }
